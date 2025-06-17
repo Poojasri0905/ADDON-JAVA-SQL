4927fe181c1f44bbd1122e60c1ca214730af991d
Poojasri0905 <poojasrirajendran09@gmail.com>
2025-06-17 15:06:56 +0530

    Update studfees.sql

--- TASK-SQL/studfees.sql
+++ TASK-SQL/studfees.sql
@@ -1,4 +1,4 @@
-mysql> create table studmark(
+mysql> create table studfees(
     -> rollno int,
     -> studname varchar(20),
     -> dept varchar(20),
@@ -6,22 +6,22 @@ mysql> create table studmark(
     -> mark int);
 Query OK, 0 rows affected (0.06 sec)
 
-mysql> insert into studmark values(1,'poojasri','ct','science',95);
+mysql> insert into studfees values(1,'poojasri','ct','science',95);
 Query OK, 1 row affected (0.05 sec)
 
-mysql> insert into studmark values(2,'abinaya','it','maths',85);
+mysql> insert into studfees values(2,'abinaya','it','maths',85);
 Query OK, 1 row affected (0.05 sec)
 
-mysql> insert into studmark values(3,'asha','cs','english',70);
+mysql> insert into studfees values(3,'asha','cs','english',70);
 Query OK, 1 row affected (0.05 sec)
 
-mysql> insert into studmark values(4,'yarshu','bca','tamil',65);
+mysql> insert into studfees values(4,'yarshu','bca','tamil',65);
 Query OK, 1 row affected (0.04 sec)
 
-mysql> insert into studmark values(5,'koushi','bba','computer',75);
+mysql> insert into studfees values(5,'koushi','bba','computer',75);
 Query OK, 1 row affected (0.01 sec)
 
-mysql> select*from studmark;
+mysql> select*from studfees;
 +--------+----------+------+----------+------+
 | rollno | studname | dept | sub      | mark |
 +--------+----------+------+----------+------+
@@ -31,31 +31,31 @@ mysql> select*from studmark;
 |      4 | yarshu   | bca  | tamil    |   65 |
 |      5 | koushi   | bba  | computer |   75 |
 +--------+----------+------+----------+------+
-mysql> alter table studmark add column fees int;
+mysql> alter table studfees add column fees int;
 Query OK, 0 rows affected (0.06 sec)
 Records: 0  Duplicates: 0  Warnings: 0
 
-mysql> update studmark set fees=50000 where mark=70;
+mysql> update studfees set fees=50000 where mark=70;
 Query OK, 1 row affected (0.03 sec)
 Rows matched: 1  Changed: 1  Warnings: 0
 
-mysql> update studmark set fees=60000 where mark=65;
+mysql> update studfees set fees=60000 where mark=65;
 Query OK, 1 row affected (0.05 sec)
 Rows matched: 1  Changed: 1  Warnings: 0
 
-mysql> update studmark set fees=45000 where mark=75;
+mysql> update studfees set fees=45000 where mark=75;
 Query OK, 1 row affected (0.03 sec)
 Rows matched: 1  Changed: 1  Warnings: 0
 
-mysql> update studmark set fees=40000 where mark=85;
+mysql> update studfees set fees=40000 where mark=85;
 Query OK, 1 row affected (0.03 sec)
 Rows matched: 1  Changed: 1  Warnings: 0
 
-mysql> update studmark set fees=30000 where mark=95;
+mysql> update studfees set fees=30000 where mark=95;
 Query OK, 1 row affected (0.06 sec)
 Rows matched: 1  Changed: 1  Warnings: 0
 
-mysql> select*from studmark;l
+mysql> select*from studfees;
 +--------+----------+------+----------+------+-------+
 | rollno | studname | dept | sub      | mark | fees  |
 +--------+----------+------+----------+------+-------+
@@ -65,7 +65,7 @@ mysql> select*from studmark;l
 |      4 | yarshu   | bca  | tamil    |   65 | 60000 |
 |      5 | koushi   | bba  | computer |   75 | 45000 |
 +--------+----------+------+----------+------+-------+
-mysql> select*from studmark order by fees asc;
+mysql> select*from studfees order by fees asc;
 +--------+----------+------+----------+------+-------+
 | rollno | studname | dept | sub      | mark | fees  |
 +--------+----------+------+----------+------+-------+
